d700f1f25d4b9b6e9dbb97c3fe2ad6a2e91eec30
AdrianF3 <AF1219@gmail.com>
2020-12-27 10:23:38 -0700

    modal implemented

--- js/AFWebDevScript.js
+++ js/AFWebDevScript.js
@@ -1,1 +1,16 @@
-console.log("running.");
+//console.log("running.");
+
+
+//Event Listener all get in touch & contact buttons
+$( document ).ready(function() {
+  document.getElementById("contact").addEventListener("click", disModal);
+  document.getElementById("getInTouch").addEventListener("click", disModal);
+});
+
+
+function disModal(evt) {
+  evt.preventDefault();
+
+  $('#contactModal').modal();
+  console.log("modal called");
+}
