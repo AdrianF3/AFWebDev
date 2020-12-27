f388973e75ac18802742270d46c0970f84271261
AdrianF3 <AF1219@gmail.com>
2020-12-27 11:29:11 -0700

    dev work
    modal improved
    menus fixed
    buttons for modal fixed

--- js/AFWebDevScript.js
+++ js/AFWebDevScript.js
@@ -1,16 +1,19 @@
 //console.log("running.");
 
 
-//Event Listener all get in touch & contact buttons
-$( document ).ready(function() {
-  document.getElementById("contact").addEventListener("click", disModal);
-  document.getElementById("getInTouch").addEventListener("click", disModal);
+
+$(document).ready(function() {
+  //Event Listener all get in touch & contact buttons
+  let elements = document.getElementsByClassName("contact-modal");
+  for (let i = 0; i < elements.length; i++) {
+      elements[i].addEventListener("click", disModal);
+  }
+
 });
 
 
 function disModal(evt) {
   evt.preventDefault();
-
   $('#contactModal').modal();
   console.log("modal called");
 }
