7d2342bd5a7eca23360de6a0882796dde4881243
AdrianF3 <AF1219@gmail.com>
2021-02-03 08:14:10 -0700

    update contact buttons

--- js/AFWebDevScript.js
+++ js/AFWebDevScript.js
@@ -1,18 +1,18 @@
 //console.log("running.");
 
 
-$(document).ready(function() {
-  //Event Listener all get in touch & contact buttons
-  let elements = document.getElementsByClassName("contact-modal");
-  for (let i = 0; i < elements.length; i++) {
-      elements[i].addEventListener("click", disModal);
-  }
-
-
-});
-
-function disModal(evt) {
-  evt.preventDefault();
-  $('#contactModal').modal();
-  console.log("modal called");
-}
+// $(document).ready(function() {
+//   //Event Listener all get in touch & contact buttons
+//   let elements = document.getElementsByClassName("contact-modal");
+//   for (let i = 0; i < elements.length; i++) {
+//       elements[i].addEventListener("click", disModal);
+//   }
+//
+//
+// });
+//
+// function disModal(evt) {
+//   evt.preventDefault();
+//   $('#contactModal').modal();
+//   console.log("modal called");
+// }
