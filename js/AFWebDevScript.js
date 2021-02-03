44389a88ee4647bc958b01272f9168fde358cd42
AdrianF3 <AF1219@gmail.com>
2021-02-02 20:28:18 -0700

    updates

--- js/AFWebDevScript.js
+++ js/AFWebDevScript.js
@@ -1,16 +1,13 @@
 //console.log("running.");
 
 
-
 $(document).ready(function() {
   //Event Listener all get in touch & contact buttons
   let elements = document.getElementsByClassName("contact-modal");
   for (let i = 0; i < elements.length; i++) {
       elements[i].addEventListener("click", disModal);
   }
-
 });
-
 
 function disModal(evt) {
   evt.preventDefault();
