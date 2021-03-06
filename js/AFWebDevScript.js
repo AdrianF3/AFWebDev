c04f7ce2fd8099bb48dba4669b70de0942584856
AdrianF3 <AF1219@gmail.com>
2021-03-06 15:57:13 -0700

    major push to update/launch

--- js/AFWebDevScript.js
+++ js/AFWebDevScript.js
@@ -1,18 +1,25 @@
 //console.log("running.");
 
 
-// $(document).ready(function() {
-//   //Event Listener all get in touch & contact buttons
-//   let elements = document.getElementsByClassName("contact-modal");
-//   for (let i = 0; i < elements.length; i++) {
-//       elements[i].addEventListener("click", disModal);
-//   }
-//
-//
-// });
-//
+$(document).ready(function() {
+  //Event Listener all get in touch & contact buttons
+  let elements = document.getElementsByClassName("contact-modal");
+  for (let i = 0; i < elements.length; i++) {
+      elements[i].addEventListener("click", disModal);
+  }
+  //Event Listener For Attribution Modal
+  //document.getElementById("attribution").addEventListener("click", attrModal);
+   $('[data-toggle="popover"]').popover({html:true});
+
+});
+
 // function disModal(evt) {
 //   evt.preventDefault();
 //   $('#contactModal').modal();
 //   console.log("modal called");
+// }
+//
+// function attrModal(evt) {
+//   //$('#attribution').modal();
+//   console.log("Attribute Called");
 // }
