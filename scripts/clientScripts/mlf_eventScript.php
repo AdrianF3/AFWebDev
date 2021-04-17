ca4732605be6203b57e23bac83d996a1a98331cf
AdrianF3 <AF1219@gmail.com>
2021-04-17 15:39:58 -0600

    Update mlf_eventScript.php

--- scripts/clientScripts/mlf_eventScript.php
+++ scripts/clientScripts/mlf_eventScript.php
@@ -30,7 +30,7 @@ error_log('Webhook verified: '.var_export($verified, true)); //check error.log t
 if($verified == 0) {
   sendMessage($data);
 } else {
-  var $errorMsg = "An error has occured at mlf_eventScript at: " . "TIMESTAMP...";
+  var $errorMsg = "An error has occured at mlf_eventScript at...";
   sendMessage($errorMsg);
 }
 
