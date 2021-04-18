68d82d9189285cb0c6f2739ac7848edf20c225b6
AdrianF3 <AF1219@gmail.com>
2021-04-18 17:53:15 -0600

    Update mlf_eventScript.php

--- scripts/clientScripts/mlf_eventScript.php
+++ scripts/clientScripts/mlf_eventScript.php
@@ -31,11 +31,11 @@ $dataObj = json_decode($data, true);
 
 $orderID = $dataObj['id'];
 $orderEmail = $dataObj['email'];
-$firstName = $dataObj['customer->first_name'];
+$firstName = $dataObj['customer']['first_name'];
 $lastName = $dataObj['customer=>last_name'];
 $testString = "Test message. The order id is: " . $orderID .
- " and the product type is: " . $orderEmail . "... the full name: " .
- $firstName . " " . $lastName . " and email: " . $orderEmail;
+ " and the email is: " . $orderEmail . "... the full name: " .
+ $firstName . " " . $lastName;
 sendMessage($testString);
 
 
@@ -71,7 +71,7 @@ $conn->close();
 function sendMessage($msgData) {
   //MAIL
   $to = 'adrianf.webdev@gmail.com';
-  $subject = 'Dev Testing 09';
+  $subject = 'Dev Testing 10';
   $headers = 'From: webDevTesting@testing.com' . "\r\n" .
      'Reply-To: Matt@MattLaneFitness' . "\r\n" .
      'X-Mailer: PHP/' . phpversion();
